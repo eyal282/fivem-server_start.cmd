ca4d519dd3274890561bbb310e002f1c5e7892b4
eyal282 <39803639+eyal282@users.noreply.github.com>
2022-01-17 11:32:34 +0200

    Update server_start.cmd
    Allows deleting crashes as well as cache.
    
    Automatically opens svgui, the batch file will not be used, instead the console is logged to a file for copy paste tactics.

--- server_start.cmd
+++ server_start.cmd
@@ -1,22 +1,38 @@
 @ECHO OFF
+
+SET path=C:/Users/Administrator/Desktop/server
+
 :choice
-set /P c=Would you like to delete your sevrer cache?[Y/N]?
-if /I "%c%" EQU "Y" goto :somewhere
-if /I "%c%" EQU "N" goto :somewhere_else
-goto :choice
-:somewhere
-echo "Deleting server cache"
-rd /s /q "D:\Youtube_Server\cache\"
-echo -
-echo Youtube_Server
-echo -
-start D:\Youtube_Server\run.cmd +exec server.cfg
-exit
-:somewhere_else
+set /P c=Would you like to delete your server cache and crashes?[Y/N]?
+if /I "%c%" EQU "Y" goto :delete_garbage
+if /I "%c%" EQU "N" goto :start_server_logger
+
+::goto :start_server_normal
+
+goto start_server_logger
+
+:delete_garbage
+echo Deleting server cache and crashes
+rd /s /q "%path%/cache"
+rd /s /q "%path%/crashes"
+goto start_server_logger
+
+:start_server_logger
 echo
 @echo off
 echo -
-echo Server starter by Dan. 
+echo Server starter by Dan, edit by Eyal282
 echo -
-start D:\Youtube_Server\run.cmd +exec server.cfg
+echo Closing this terminal closes the server, the file will be found in %path%/server-terminal.txt.
+echo -
+echo You can send commands here, but you cannot see them at all. If the big view screen is closed, blindly run this command: svgui
+echo -
+cd %path%
+
+
+%path%/FXServer.exe +svgui +set sv_licenseKey licensekey +exec server.cfg +set onesync_population true +set onesync_enableBeyond true > %path%/server-terminal.txt
+
 exit
+
+
+
